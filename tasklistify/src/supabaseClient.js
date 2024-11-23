e499956734f8399a3486a2b2e889ac794db972e3
Ben Lupin <benlupin@icloud.com>
2024-11-22 20:14:03 -0400

    changed env to root dr

--- tasklistify/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://oxjqldoghtqrhxykpjmj.supabase.co'; // Replace with your Supabase URL
-const supabaseKey = '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94anFsZG9naHRxcmh4eWtwam1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1MDczNzEsImV4cCI6MjA0NTA4MzM3MX0.s-uwrca4Y0UnrBEavhXigPcQLP9RIKJGPW0b1HQI57Y>'; // Replace with your Supabase anon key
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
