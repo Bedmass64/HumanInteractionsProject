8d30b62dd3587814fec439b69a6ee179e1ecf6fa
Ben Lupin <benlupin@icloud.com>
2024-11-24 17:11:37 -0400

    Re-Commented Code

--- tasklistify/src/App.js
+++ tasklistify/src/App.js
@@ -1,27 +1,3 @@
-// // src/App.js
-// "use client";
-
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import WelcomePage from './WelcomePage';
-// import TaskListifyPage from './TaskListifyPage';
-// import LoginPage from './LoginPage';
-// import CreateAccount from './CreateAccount';
-
-
-// export default function App() {
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<WelcomePage />} />
-//                 <Route path="/tasks" element={<TaskListifyPage />} />
-//                 <Route path="/create-account" element={<CreateAccount />} />
-//                 <Route path="/login" element={<LoginPage />} />
-//             </Routes>
-//         </Router>
-//     );
-// }
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import TaskListifyPage from './TaskListifyPage';
--- tasklistify/src/TaskListify.js
+++ tasklistify/src/TaskListify.js
@@ -1,29 +1,3 @@
-// import TaskList from './TaskList';
-
-// export default class TaskListify {
-//   constructor() {
-//     this.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//     this.taskLists = this.daysOfWeek.reduce((acc, day) => {
-//       acc[day] = new TaskList();
-//       return acc;
-//     }, {});
-//   }
-
-//   addTaskToDays(days, task) {
-//     days.forEach(day => {
-//       if (this.taskLists[day]) {
-//         this.taskLists[day].addTask(task);
-//       }
-//     });
-//   }
-
-//   getTaskLists() {
-//     return this.taskLists;
-//   }
-// }
-
-
-// TaskListify.js
 class TaskListify {
   constructor() {
     this.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
